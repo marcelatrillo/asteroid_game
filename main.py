f36a5c6fff1cbb80482066939e45cddd308187bd
marcelatrillo <marcelatrillordz@gmail.com>
2026-05-20 16:58:39 +0200

    feature_player

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from logger import log_state
+from player import Player
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0.0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+    while True: 
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            pass
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60)/1000
+        player.update(dt)
+        
+
+if __name__ == "__main__":
+    main()
