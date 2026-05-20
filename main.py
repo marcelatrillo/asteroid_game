73f806052268aa132c26cfdd565cac4fc4450eb7
marcelatrillo <marcelatrillordz@gmail.com>
2026-05-20 18:05:37 +0200

    feature_asteroids

--- main.py
+++ main.py
@@ -2,28 +2,46 @@ import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+
     while True: 
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pass
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
+        
         dt = clock.tick(60)/1000
-        player.update(dt)
+        
+        
         
 
 if __name__ == "__main__":
